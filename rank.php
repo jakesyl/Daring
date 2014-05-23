0fcf0ed2a70b0fcec00e15ab5f444d56a9530a4b
jakesyl <jakesyl@gmail.com>
2014-05-22 22:17:06 -0400

    useless shit might work for alex tho for info.....
    delivery

---
+++ rank.php
@@ -0,0 +1,27 @@
+<?php
+/*class link{
+    __init__(self/*,whatever we take from the mysql database as well*/){
+//        $id = rand(21)
+//        $upvotes = rand(21)
+//        $downvotes = rand(21)
+//        $pledge = rand(21)//replace with new data poinmts at a later date
+//         $age =
+// }
+//}
+/*def datextraction(){//this method is complete shit it's only useful for delivering info now commenting out
+$database = ["123, 12, 10, 49.48,12"]//id,upvotes,downvotes,plege,age
+$finval = [];//final values
+    for link in database{
+    $data = explode(",", database);
+    $id = data[0];
+    $votes = data[1]-data[2];
+    $pledge = data[3];
+    $age = data[4];
+    array_push($database.",".$id.","$votes.",".$pledge.",".$age);
+}
+}
+info, datextraction()
+    //return format id,votes(sum),pledge 
+*/
+        
+?> 
